8048a47a074081386fae6fee1bf3f29531bd301f
JeffersonAlves12 <jeffersonfelix@alunos.utfpr.edu.br>
2024-10-15 19:52:15 -0300

    ta-osso onboarding

---
+++ ta_osso/lib/Onboarding/onboarding_items.dart
@@ -0,0 +1,24 @@
+import 'package:ta_osso/Onboarding/onboarding_info.dart';
+
+class OnboardingItems {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+      title: 'Organize Suas Finanças',
+      description:
+          'Acompanhe todas as suas transações em um só lugar, categorizando facilmente seus gastos.',
+      image: 'assets/images/onboarding1.png',
+    ),
+    OnboardingInfo(
+      title: 'Relatórios Personalizados',
+      description:
+          'Visualize relatórios claros que mostram onde você está gastando seu dinheiro',
+      image: 'assets/images/onboarding2.png',
+    ),
+    OnboardingInfo(
+      title: 'Defina Seus Objetivos',
+      description:
+          'Estabeleça metas financeiras e monitore seu progresso para atingir seus sonhos',
+      image: 'assets/images/onboarding3.png',
+    ),
+  ];
+}
