6d035df67bccd8113c9bad66bb79897c9ecddebc
JeffersonAlves12 <jeffersonfelix@alunos.utfpr.edu.br>
2024-10-17 09:56:03 -0300

    Onboarding completo

--- ta_osso/lib/Onboarding/onboarding_view.dart
+++ ta_osso/lib/Onboarding/onboarding_view.dart
@@ -12,50 +12,13 @@ class OnboardingView extends StatefulWidget {
 class _OnboardingViewState extends State<OnboardingView> {
   final controller = OnboardingItems();
   final pageController = PageController();
+  bool isLastPage = false;
   int currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomSheet: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            TextButton(
-              onPressed: () =>
-                  pageController.jumpToPage(controller.items.length - 1),
-              child: const Text('Pular'),
-            ),
-            SmoothPageIndicator(
-              controller: pageController,
-              count: controller.items.length,
-              onDotClicked: (index) => pageController.animateToPage(
-                index,
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.easeIn,
-              ),
-              effect: const WormEffect(
-                dotHeight: 12,
-                dotWidth: 12,
-                activeDotColor: Color.fromARGB(255, 230, 200, 33),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                if (currentPage < controller.items.length - 1) {
-                  pageController.nextPage(
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeIn,
-                  );
-                }
-              },
-              child: const Text('Próximo'),
-            ),
-          ],
-        ),
-      ),
+      bottomSheet: isLastPage ? getStarted(context) : buildBottomNavigation(),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 15),
         child: PageView.builder(
@@ -64,35 +27,102 @@ class _OnboardingViewState extends State<OnboardingView> {
           onPageChanged: (index) {
             setState(() {
               currentPage = index;
+              isLastPage = index == controller.items.length - 1;
             });
           },
           itemBuilder: (context, index) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(controller.items[index].image, height: 450),
-                const SizedBox(height: 15),
-                Text(
-                  controller.items[index].title,
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Text(
-                  controller.items[index].description,
-                  style: const TextStyle(
-                    color: Colors.grey,
-                    fontSize: 17,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
-            );
+            return buildOnboardingPage(index);
           },
         ),
       ),
     );
   }
+
+  Widget buildOnboardingPage(int index) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(controller.items[index].image, height: 450),
+        const SizedBox(height: 15),
+        Text(
+          controller.items[index].title,
+          style: const TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 15),
+        Text(
+          controller.items[index].description,
+          style: const TextStyle(
+            color: Colors.grey,
+            fontSize: 17,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget buildBottomNavigation() {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextButton(
+            onPressed: () =>
+                pageController.jumpToPage(controller.items.length - 1),
+            child: const Text('Pular'),
+          ),
+          SmoothPageIndicator(
+            controller: pageController,
+            count: controller.items.length,
+            onDotClicked: (index) => pageController.animateToPage(
+              index,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeIn,
+            ),
+            effect: const WormEffect(
+              dotHeight: 12,
+              dotWidth: 12,
+              activeDotColor: Color.fromARGB(255, 230, 200, 33),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              if (currentPage < controller.items.length - 1) {
+                pageController.nextPage(
+                  duration: const Duration(milliseconds: 500),
+                  curve: Curves.easeIn,
+                );
+              }
+            },
+            child: const Text('Próximo'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget getStarted(BuildContext context) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 60,
+    color: Colors.orange,
+    child: TextButton(
+      onPressed: () {
+        // Nevar para a tela de login ou home
+      },
+      child: const Text(
+        'Começar Agora',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+    ),
+  );
 }
