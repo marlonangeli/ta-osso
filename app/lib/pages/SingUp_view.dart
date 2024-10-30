1d1b4ce59e7655109567d6cd03d53fb59d468e3a
renata kaneko <renatakaneko@alunos.utfpr.edu.br>
2024-10-29 22:12:02 -0300

    Tela de cadastro amarela

---
+++ app/lib/pages/SingUp_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SingUpView extends StatefulWidget {
+  const SingUpView({super.key});
+
+  @override
+  State<SingUpView> createState() => _SingUpViewState();
+}
+
+class _SingUpViewState extends State<SingUpView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+    );
+  }
+}
