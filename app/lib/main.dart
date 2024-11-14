0842d0c841807e7fe48419252244c7f665ccfd44
Marlon Angeli <iam@marlonangeli.com.br>
2024-11-14 14:30:03 -0300

    add auth services and update pages
    add 3 features, signin, createUser and recoverPassword with email

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ta_osso/pages/Onboarding/onboarding_view.dart';
-import 'package:ta_osso/pages/Auth/login_view.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // TODO: deixar mais bonito
   runApp(const MyApp());
 }
 
@@ -15,7 +17,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Ta Osso',
       theme: ThemeData(
-        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
         useMaterial3: true,
       ),
