e6212d132f7e9457e4428feaa8f7052158194c37
Marlon Angeli <iam@marlonangeli.com.br>
2024-11-14 14:36:06 -0300

    prepare for merge

--- app/lib/pages/Onboarding/onboarding_view.dart
+++ app/lib/pages/Onboarding/onboarding_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:ta_osso/pages/Onboarding/onboarding_items.dart';
 import 'package:ta_osso/pages/Auth/login_view.dart';
-import 'package:ta_osso/pages/login_screen.dart';
 
 class OnboardingView extends StatefulWidget {
   const OnboardingView({super.key});
